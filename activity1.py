a6aebb29905abb1c9701baa2d551ce92d69b08b4
EmaaZmaker <emaazbinammar@gmail.com>
2025-03-15 11:24:01 +0400

    activity1

---
+++ activity1.py
@@ -0,0 +1,14 @@
+x=5.00
+if type(x) is not float:
+    print("true")
+else:
+    print("false")
+
+a=20
+b=20
+if a is b:
+    print("A and b are equal")
+c=30
+if c is not a:
+    print("a and c are not equal")
+
